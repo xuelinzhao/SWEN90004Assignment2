707529c00890c3b01ce9959f2abbd5e9fd63a3a9
xuelinzhao <zhaoxuelin1994@gmail.com>
2018-05-12 11:16:11 +1000

    Initial

---
+++ src/Patch.java
@@ -0,0 +1,16 @@
+
+public class Patch {
+	public int ID;
+	public int[][] location;
+	public float grains;
+	public float growCount;
+	
+	public void grow(int numOfGrow) {
+		
+	}
+	
+	public void harvest() {
+		
+	}
+	
+}
---
+++ src/Person.java
@@ -0,0 +1,25 @@
+
+public class Person {
+	public int ID;
+	public int[][] location;
+	public int age;
+	public float wealth;
+	public int vision;
+	private int lifeExpectancy;
+	private float metabolism;
+	
+	public void harvest(int grains) {
+		
+	}
+	
+	public boolean eat() {
+		
+		return false;
+	}
+	
+	public boolean age() {
+		
+		return false;
+	}
+	
+}
