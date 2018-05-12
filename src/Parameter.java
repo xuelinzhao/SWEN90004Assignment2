3b04192adc526831b92c725be56876f4f39178a9
xuelinzhao <zhaoxuelin1994@gmail.com>
2018-05-12 11:43:31 +1000

    Some parameters

--- src/Parameter.java
+++ src/Parameter.java
@@ -1,4 +1,23 @@
 
 public class Parameter {
+	public final static int WORLDWIDTH = 50;
+	public final static int WORLDHEIGHT = 50;
+	public final static int MAXGRAIN = 50;
+	
+	
+	public int numOfPeople;
+
+	public int maxVision;
+	public int maxMetabolism;
+	public int minLifeSpan;
+	public int maxLifeSpan;
+	public float percentBestLand;
+	
+	public int growInterval;
+	public int numOfGrow;
+	
+	
+	
+	
 	
 }
--- src/World.java
+++ src/World.java
@@ -1,9 +1,40 @@
 
 public class World {
-
+	
+	public int numberOfPeople; // Move to Parameter?
+	
+	public Person[] richPeople;
+	public Person[] middlePeople;
+	public Person[] poorPeople;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void tick() {
+		
+	}
+	
+	public void move(Person p) {
+		
+	}
+	
+	public void die(Person p) {
+		
+	}
+
+	public void writeRecord() {
+		
+	}
+	
+	public void lorenzOutput() {
+		
+	}
+	
+	public void giniOutput() {
+		
+	}
+	
 
 }
